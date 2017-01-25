6b7f33eb2804aa3050423356050a4c44807b34ac
raphaelpare <rapha.pare@gmail.com>
2017-01-25 19:02:55 +0100

    Full

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RedisChat 1.0");
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-16112.c11.us-east-1-3.ec2.cloud.redislabs.com:16112");
+            ISubscriber sub = redis.GetSubscriber();
+
+            sub.Subscribe("messages", (channel, message) => {
+                Console.WriteLine((string)message);
+                Console.Write("> ");
+            });
+
+            Console.Write("Enter your name : ");
+            var name = Console.ReadLine();
+
+            sub.Publish("messages", "["+ DateTime.Now +"] " + name + " entered the channel");
+            
+            while(true) {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                sub.Publish("messages", "["+ DateTime.Now +"] " + name + " : " + input);
+            }
+        }
+    }
+}
